5ba70671af0bef834ff8402971d7a46d5b586350
Jake Heft <jakeheft@gmail.com>
2020-10-06 19:31:22 -0600

    Add comments for root index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #do I want the following (per odin project)
+  # root to: "welcome#index"
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
 end
