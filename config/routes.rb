623b2ae7adee0b16067996e213ca53e5801e2fc6
Jake Heft <jakeheft@gmail.com>
2020-10-06 21:43:49 -0600

    Add new functionality for shelters

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root to: "welcome#index"
   get '/', to: 'welcome#index'
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
+  # get '/shelters/:id', to: 'shelters#show'
 end
