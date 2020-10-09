90317ad044d65d03ee3f9b0c8bc8c5138c6a8c0d
Jake Heft <jakeheft@gmail.com>
2020-10-08 19:33:09 -0600

    Add test for deleting shelter, user story 6

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -21,3 +21,36 @@ describe "When I visit '/shelters/:id'" do
     expect(page).to have_content("#{shelter_1.zip}")
   end
 end
+
+# When I visit a shelter show page
+# Then I see a link to delete the shelter
+# When I click the link "Delete Shelter"
+# Then a 'DELETE' request is sent to '/shelters/:id',
+# the shelter is deleted,
+# and I am redirected to the shelter index page where I no longer see this shelter
+describe "When I visit a shelter show page I see a link to delete the shelter" do
+  it "When I click the link I'm redirected to '/shelters' where the shelter is gone" do
+    shelter_1 = Shelter.create(
+      name: "Denver Shelter",
+      address: "123 Main St.",
+      city: "Denver",
+      state: "CO",
+      zip: "80211"
+    )
+    shelter_2 = Shelter.create(
+      name: "Happy Animals",
+      address: "888 2nd Ave.",
+      city: "CO Springs",
+      state: "CO",
+      zip: "80333"
+    )
+
+    visit "/shelters/#{shelter_2.id}"
+
+    click_link("Delete Shelter")
+
+    expect(current_path).to eq("/shelters")
+    expect(page).to has_no_content("Happy Animals")
+    expect(page).to have_content("Denver Shelter")
+  end
+end
