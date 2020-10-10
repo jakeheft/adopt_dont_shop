6c45cc2f04767cc1024866485c73048f6db0246f
Jake Heft <jakeheft@gmail.com>
2020-10-10 14:04:27 -0600

    Add migration for relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_170311) do
+ActiveRecord::Schema.define(version: 2020_10_10_193110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_10_10_170311) do
     t.string "age"
     t.string "sex"
     t.string "shelter"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
   create_table "shelters", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2020_10_10_170311) do
     t.string "zip"
   end
 
+  add_foreign_key "pets", "shelters"
 end
