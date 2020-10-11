5bccdccdb8097d35c182e33944d01f8ed92bb161
Jake Heft <jakeheft@gmail.com>
2020-10-11 13:15:26 -0600

    Add test to delete pet

---
+++ spec/features/pets/delete_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+# As a visitor
+# When I visit a pet show page
+# Then I see a link to delete the pet "Delete Pet"
+# When I click the link
+# Then a 'DELETE' request is sent to '/pets/:id',
+# the pet is deleted,
+# and I am redirected to the pet index page where I no longer see this pet
+
+RSpec.describe "As a visitor" do
+  describe "When I visit a pet show page" do
+    describe "I click a link to 'Delete Pet'" do
+      it "Directs me to the pet index page where the pet is gone" do
+        shelter = Shelter.create(
+          name: "Denver Shelter",
+          address: "123 Main St.",
+          city: "Denver",
+          state: "CO",
+          zip: "80211")
+        pet_1 =Pet.create(
+          image: "https://images.unsplash.com/photo-1455526050980-d3e7b9b789a4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80",
+          name: "Cosmo",
+          description: "Cute, cuddly, awesome",
+          age: "8",
+          sex: "Male",
+          status: "Adoptable",
+          shelter_name: shelter.name,
+          shelter: shelter)
+        pet_2 =Pet.create(
+          image: "https://images.unsplash.com/photo-1455526050980-d3e7b9b789a4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80",
+          name: "Frank",
+          description: "Mean",
+          age: "3",
+          sex: "Male",
+          status: "Adoptable",
+          shelter_name: shelter.name,
+          shelter: shelter)
+
+        visit "/pets/#{pet_1.id}"
+
+        click_link "Delete"
+
+        expect(current_path).to eq("/pets")
+        expect(page).to have_no_content("Cosmo")
+        expect(page).to have_no_content("Cute, cuddly, awesome")
+        expect(page).to have_content("Frank")
+        expect(page).to have_content("Mean")
+      end
+    end
+  end
+end
