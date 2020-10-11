3413420cb4936c25b338aa181d69d77c060aa958
Jake Heft <jakeheft@gmail.com>
2020-10-11 10:12:38 -0600

    Add shelter pets controller and updated files accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,11 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
-  get '/shelters/:id/pets', to: 'shelters#pets'
-  get '/shelters/:shelter_id/pets/new', to: 'shelters#new_pet'
-  post '/shelters/:shelter_id/pets', to: 'shelters#create_pet'
+
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
+
+  get '/shelters/:id/pets', to: 'shelter_pets#index'
+  get '/shelters/:shelter_id/pets/new', to: 'shelter_pets#new'
+  post '/shelters/:shelter_id/pets', to: 'shelter_pets#create'
 end
