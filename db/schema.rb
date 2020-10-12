0538d771f4ac3766bdd8b0a99ed56961758a4650
Jake Heft <jakeheft@gmail.com>
2020-10-12 17:41:44 -0600

    Add users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_150814) do
+ActiveRecord::Schema.define(version: 2020_10_12_234027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2020_10_11_150814) do
   end
 
   create_table "shelters", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "city"
