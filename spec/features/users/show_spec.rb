3c2c53f8780dd5bfa23717b39a964e523b342cf0
Jake Heft <jakeheft@gmail.com>
2020-10-12 17:49:29 -0600

    Add test see user show page

---
+++ spec/features/users/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+# As a visitor
+# When I visit a User's show page
+# Then I see all that User's information
+# Including the User's data
+RSpec.describe "As a visitor" do
+  describe "When I visit a User's show page" do
+    it "Then I see all of that User's data" do
+      user = User.create(
+        name: "Jake",
+        address: "222 1st St.",
+        city: "Denver",
+        state: "CO",
+        zip: "80202"
+      )
+
+      visit "/users/#{user.id}"
+
+      expect(page).to have_content("Jake")
+      expect(page).to have_content("222 1st St.")
+      expect(page).to have_content("Denver")
+      expect(page).to have_content("CO")
+      expect(page).to have_content("80202")
+    end
+  end
+end
