33a40ac1ed3c6b117153ccfeb6055699f471e38f
Nick King <nickmaxking@gmail.com>
2020-10-13 18:54:56 -0600

    Add relationship to shelters through reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,4 @@
-class Review < ApplicationRecord
+.class Review < ApplicationRecord
   belongs_to :shelter
   belongs_to :user
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   describe "relationships" do
     it { should have_many :reviews}
+    it { should have_many(:shelters).through(:reviews) }
   end
 end
