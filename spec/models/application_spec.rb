a103f14377a5a5b94cc629be6341eeaac1451367
Jake Heft <jakeheft@gmail.com>
2020-10-15 18:33:11 -0600

    Update model test with proper validations

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Application, type: :model do
   describe "validations" do
     it { should validate_presence_of :user_id }
     it { should validate_presence_of :pet_id }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :status }
   end
 
   describe "relationships" do
