a9dd4d1b2859dd67c6bab089715aee71de72504d
Jake Heft <jakeheft@gmail.com>
2020-10-16 17:24:25 -0600

    Add test for application show

---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "When I visit an applications show page '/applications/:id'" do
+    it "Then I can see user name, user address, description, pets applied for, and status" do
+      user = User.create!(
+        name: "Jake",
+        address: "222 1st St.",
+        city: "Denver",
+        state: "CO",
+        zip: "80202"
+      )
+      shelter = Shelter.create(
+        name: "Denver Shelter",
+        address: "123 Main St.",
+        city: "Denver",
+        state: "CO",
+        zip: "80211"
+      )
+      pet_1 = Pet.create(
+        image: "https://images.unsplash.com/photo-1455526050980-d3e7b9b789a4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80",
+        name: "Cosmo",
+        age: "8",
+        sex: "Male",
+        status: "Adoptable",
+        shelter: shelter
+      )
+      pet_2 = Pet.create(
+        image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        name: "Zoey",
+        age: "9",
+        sex: "Female",
+        status: "Adoptable",
+        shelter: shelter
+      )
+      application = Application.create(
+        user: user,
+        pet: [pet_1, pet_2],
+        description: "I am awesome",
+        status: "In Progress"
+      )
+
+      visit "/applications/#{application.id}"
+
+      expect(page).to have_content(user.name)
+      expect(page).to have_content(user.address)
+      expect(page).to have_content(application.description)
+      expect(page).to have_content(application.status)
+      expect(page).to have_link("Cosmo")
+      expect(page).to have_link("Zoey")
+    end
+  end
+end
