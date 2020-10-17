64c12791bcc96f066fd97fe65c46dfdc62c573da
Jake Heft <jakeheft@gmail.com>
2020-10-17 14:30:23 -0600

    Add test for application submission

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -162,22 +162,89 @@ describe "As a visitor" do
 
         visit "/applications/#{application.id}"
 
+        fill_in "pet_name", with: "cosmo"
+
+        click_button "Find Pets"
         click_button "Adopt this Pet"
 
         expect(current_path).to eq("/applications/#{application.id}")
 
         within '#added-pets' do
-          expect(page).to have_content("Cosmo")
-          expect(page).to have_content(8)
-          expect(page).to have_content("Male")
-          expect(page).to have_content("Adoptable")
+          expect(page).to have_link("Cosmo")
+
+          click_link "Cosmo"
+
+          expect(current_path).to eq("/pets/#{pet_1.id}")
         end
       end
     end
   end
 end
-# And I see the names Pets that matches my search
-# Then next to each Pet's name I see a button to "Adopt this Pet"
-# When I click one of these buttons
-# Then I am taken back to the application show page
-# And I see the Pet I want to adopt listed on this application
+
+describe "As a visitor" do
+  describe "When I visit an application's show page and I have added one or more pets to the application" do
+    describe "Then I see a section to submit my application where I can input a description" do
+      describe "When I fill in that input and I click a button to submit this application" do
+        it "Then I am taken back to the application's show page, I see that the application is 'Pending', I see all the pets that I want to adopt, and I do not see a section to add more pets to this application" do
+          user = User.create!(
+            name: "Jake",
+            address: "222 1st St.",
+            city: "Denver",
+            state: "CO",
+            zip: "80202"
+          )
+          shelter = Shelter.create(
+            name: "Denver Shelter",
+            address: "123 Main St.",
+            city: "Denver",
+            state: "CO",
+            zip: "80211"
+          )
+          pet_1 = Pet.create(
+            image: "https://images.unsplash.com/photo-1455526050980-d3e7b9b789a4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80",
+            name: "Cosmo",
+            age: "8",
+            sex: "Male",
+            status: "Adoptable",
+            shelter: shelter
+          )
+          pet_2 = Pet.create(
+            image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+            name: "Zoey",
+            age: "9",
+            sex: "Female",
+            status: "Adoptable",
+            shelter: shelter
+          )
+          application = Application.create(
+            user: user,
+            status: "In Progress"
+          )
+          PetApplication.create(
+            pet: pet_1,
+            application: application
+          )
+          PetApplication.create(
+            pet: pet_2,
+            application: application
+          )
+
+          visit "/applications/#{application.id}"
+
+          within '#apply' do
+            fill_in "Description", with: "I am more awesome"
+
+            click_button "Submit Application"
+          end
+
+          expect(current_path).to eq("/applications/#{application.id}")
+
+          expect(page).to have_content("Pending")
+          expect(page).to have_link(pet_1.name)
+          expect(page).to have_link(pet_2.name)
+          expect(page).to have_no_content("Add a Pet to this Application")
+        end
+      end
+    end
+  end
+end
