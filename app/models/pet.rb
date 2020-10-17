bf84af80651bad0127a42527ba97dcfe2bbd4083
Jake Heft <jakeheft@gmail.com>
2020-10-16 18:06:36 -0600

    Reconfigure models with new relationships for pet applications

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
-  has_many :applications
-  has_many :users, through: :applications
+  has_many :pet_applications
+  has_many :applications, through: :pet_applications
+  # has_many :users, through: :applications
 
   validates_presence_of :image, :name, :age, :sex
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :shelters, through: :reviews
   has_many :applications
-  has_many :pets, through: :applications
+  # has_many :pets, through: :applications
 end
