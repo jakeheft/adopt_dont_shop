478a97f2d08d25450fe72a20ec54db97ff64cff9
Jake Heft <jakeheft@gmail.com>
2020-10-16 19:14:19 -0600

    Add full address method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :shelters, through: :reviews
   has_many :applications
   # has_many :pets, through: :applications
+
+  def full_address
+    "#{self.address}, #{self.city}, #{self.state}, #{self.zip}"
+  end
 end
