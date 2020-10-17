64160c2c4ec02a55e729d6ef190322ae739cccb5
Jake Heft <jakeheft@gmail.com>
2020-10-16 19:11:59 -0600

    Add model test for full address method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,3 +8,22 @@ RSpec.describe User, type: :model do
     # it { should have_many(:pet_applications).through(:applications) }
   end
 end
+
+describe "instance methods" do
+  it "#full_address" do
+    user = User.create!(
+      name: "Jake",
+      address: "222 1st St.",
+      city: "Denver",
+      state: "CO",
+      zip: "80202"
+    )
+    application = Application.create(
+      user: user,
+      description: "I am awesome",
+      status: "In Progress"
+    )
+
+    expect(user.full_address).to eq("222 1st St., Denver, CO, 80202")
+  end
+end
