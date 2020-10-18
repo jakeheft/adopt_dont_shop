4451b316fd6d8200dc91af766fc388e759a7a6bd
Nick King <nickmaxking@gmail.com>
2020-10-18 13:55:52 -0600

    Add update action. Modify create action to create with default pet_application_status

--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,11 +1,23 @@
 class PetApplicationsController < ApplicationController
   def create
-    PetApplication.create(pet_app_params)
+    PetApplication.create(
+      pet_id: params[:pet_id],
+      application_id: params[:application_id],
+      pet_application_status: "Pending"
+    )
     redirect_to "/applications/#{params[:application_id]}"
   end
 
-  private
-  def pet_app_params
-    params.permit(:pet_id, :application_id)
+  def update
+    pet_app = PetApplication.where(application_id: params[:application_id]).where(pet_id: params[:pet_id])
+
+    pet_app.update(pet_application_status: "Approved")
+
+    redirect_to "/admin/applications/#{params[:application_id]}"
   end
+
+  # private
+  # def pet_app_params
+  #   params.permit(:pet_id, :application_id)
+  # end
 end
