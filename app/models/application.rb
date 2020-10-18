bffb76029bd79c4fe045b79fc6fa2e841c42d071
Nick King <nickmaxking@gmail.com>
2020-10-18 14:52:48 -0600

    Add status check method to update application status to approved for rejected

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,17 @@ class Application < ApplicationRecord
   has_many :pets, through: :pet_applications
 
   validates_presence_of :user_id, :status
+
+  def status_check
+    statuses = pet_applications.map do |pet_app|
+      pet_app.pet_application_status
+    end
+    if statuses.include?("Rejected")
+      self.status = "Rejected"
+    elsif statuses.include?("Pending")
+      self.status = "Pending"
+    else
+      self.status = "Approved"
+    end
+  end
 end
