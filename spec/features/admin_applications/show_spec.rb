62e051f46f3b959a9bd3c9489ef44a565fd712fa
Nick King <nickmaxking@gmail.com>
2020-10-18 15:47:02 -0600

    Add test for admin applications view changing application status when pet application statuses are changed

--- spec/features/admin_applications/show_spec.rb
+++ spec/features/admin_applications/show_spec.rb
@@ -147,3 +147,70 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe  "When I visit an admin application show page" do
+    describe "And I approve all pets for an application" do
+      it "I go back to the admin application show page to see application's status is'Approved'" do
+        user = User.create!(
+          name: "Jake",
+          address: "222 1st St.",
+          city: "Denver",
+          state: "CO",
+          zip: "80202"
+        )
+        shelter = Shelter.create(
+          name: "Denver Shelter",
+          address: "123 Main St.",
+          city: "Denver",
+          state: "CO",
+          zip: "80211"
+        )
+        pet_1 = Pet.create(
+          image: "https://images.unsplash.com/photo-1455526050980-d3e7b9b789a4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80",
+          name: "Cosmo",
+          age: "8",
+          sex: "Male",
+          status: "Adoptable",
+          shelter: shelter
+        )
+        pet_2 = Pet.create(
+          image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+          name: "Zoey",
+          age: "9",
+          sex: "Female",
+          status: "Adoptable",
+          shelter: shelter
+        )
+        application = Application.create(
+          user: user,
+          description: "I am awesome",
+          status: "Pending"
+        )
+        PetApplication.create(
+          pet: pet_1,
+          application: application,
+          pet_application_status: "Pending"
+        )
+        PetApplication.create(
+          pet: pet_2,
+          application: application,
+          pet_application_status: "Pending"
+        )
+
+        visit "/admin/applications/#{application.id}"
+
+
+        within("#pet-#{pet_1.id}") do
+          click_button "Approve Pet"
+        end
+        within("#pet-#{pet_2.id}") do
+          click_button "Approve Pet"
+        end
+
+        expect(page).to have_content("Application status: Approved")
+
+      end
+    end
+  end
+end
