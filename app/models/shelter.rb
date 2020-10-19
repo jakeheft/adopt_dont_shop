3f0dcae1f64c271350fd3a460bd906b17d6087af
Nick King <nickmaxking@gmail.com>
2020-10-19 14:42:38 -0600

    Add average_reviews instance method

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -4,4 +4,13 @@ class Shelter < ApplicationRecord
   has_many :users, through: :reviews
 
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def total_pets
+    pets.count
+  end
+
+  def average_reviews
+    total_ratings = reviews.sum { |review| review.rating }
+    total_ratings.to_f / reviews.count
+  end
 end
