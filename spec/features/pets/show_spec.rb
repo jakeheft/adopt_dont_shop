7f527df6cfa012fb04f9126b5d28be76d4e13bb1
Jake Heft <jakeheft@gmail.com>
2020-10-20 15:06:55 -0600

    Add test for not being able to delete accepted or approved pet

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -58,3 +58,81 @@ describe "As a visitor" do
     end
   end
 end
+
+describe "As a visitor" do
+  describe "If a pet has an approved application on them" do
+    it "I can not delete that pet" do
+      shelter_1 = Shelter.create(
+        name: "Denver Shelter",
+        address: "123 Main St.",
+        city: "Denver",
+        state: "CO",
+        zip: "80211"
+      )
+      shelter_2 = Shelter.create(
+        name: "Boulder Shelter",
+        address: "123 1st St.",
+        city: "Boulder",
+        state: "CO",
+        zip: "80211"
+      )
+      pet_1 = Pet.create(
+        image: "https://images.unsplash.com/photo-1455526050980-d3e7b9b789a4?ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80",
+        name: "Cosmo",
+        age: "8",
+        sex: "Male",
+        status: "Adoptable",
+        shelter: shelter_1
+      )
+      pet_2 = Pet.create(
+        image: "https://images.unsplash.com/photo-1534361960057-19889db9621e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        name: "Zoey",
+        age: "9",
+        sex: "Female",
+        status: "Adoptable",
+        shelter: shelter_2
+      )
+      user = User.create!(
+        name: "Jake",
+        address: "222 1st St.",
+        city: "Denver",
+        state: "CO",
+        zip: "80202"
+      )
+      application_1 = Application.create(
+        user: user,
+        status: "Rejected"
+      )
+      application_2 = Application.create(
+        user: user,
+        status: "Approved"
+      )
+      pet_app_1 = PetApplication.create(
+        pet: pet_1,
+        application: application_1,
+        pet_application_status: "Rejected"
+      )
+      pet_app_2 = PetApplication.create(
+        pet: pet_2,
+        application: application_2,
+        pet_application_status: "Approved"
+      )
+
+      visit "/pets/#{pet_1.id}"
+
+      click_link "Delete Pet"
+
+      expect(current_path).to eq("/pets")
+      expect(page).to have_no_content(pet_1.name)
+      expect(PetApplication.where(id: pet_app_1.id)).to eq([])
+
+      visit "/pets/#{pet_2.id}"
+
+      click_link "Delete Pet"
+
+      expect(current_path).to eq("/pets/#{pet_2.id}")
+      expect(PetApplication.where(id: pet_app_2.id)).to eq([pet_app_2])
+      expect(page).to have_content("This pet cannot be deleted because it is in the process of adoption")
+    end
+  end
+end
