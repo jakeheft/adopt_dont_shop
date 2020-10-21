29ceadd2767d58022e71cfce8c820bca93a8cc51
Jake Heft <jakeheft@gmail.com>
2020-10-21 15:53:32 -0600

    Remove comment lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :shelters, through: :reviews
   has_many :applications
-  # has_many :pets, through: :applications
 
   def full_address
     "#{address}, #{city}, #{state}, #{zip}"
