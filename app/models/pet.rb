ac375cac7e76637b71b6433a90a11e82b7279ea7
Jake Heft <jakeheft@gmail.com>
2020-10-21 12:16:38 -0600

    Refactor find pets method to use activerecord method vs ruby

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,14 +2,11 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   has_many :pet_applications
   has_many :applications, through: :pet_applications
-  # has_many :users, through: :applications
 
   validates_presence_of :image, :name, :age, :sex
 
   def self.find_pets(pet_name)
-    Pet.all.find_all do |pet|
-      pet.name.downcase.include?(pet_name.downcase)
-    end
+    Pet.where("name like ?", "%#{pet_name}%")
   end
 
   def approved_anywhere?
